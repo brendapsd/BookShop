359bbde3b519222dcf5e1cc708652faf171ddf35
brendapsd <brenda.sd.15@gmail.com>
2019-10-16 16:56:54 -0500

    Updates

---
+++ precache-manifest.f1819e961f9e5d6f0f73fa75ec08a4e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7b64afaf909bba99f873b7c478173a8",
+    "url": "//brendapsd.github.io/BookShop/index.html"
+  },
+  {
+    "revision": "f98493a1adf6ff8c9c9a",
+    "url": "//brendapsd.github.io/BookShop/static/css/main.6adc3191.chunk.css"
+  },
+  {
+    "revision": "eab7fec42f9d25aa4a7e",
+    "url": "//brendapsd.github.io/BookShop/static/js/2.2df6cd18.chunk.js"
+  },
+  {
+    "revision": "f98493a1adf6ff8c9c9a",
+    "url": "//brendapsd.github.io/BookShop/static/js/main.38f8849f.chunk.js"
+  },
+  {
+    "revision": "c96897e205b6061978e2",
+    "url": "//brendapsd.github.io/BookShop/static/js/runtime-main.431d7c54.js"
+  }
+]);
